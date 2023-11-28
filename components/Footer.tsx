9f132dce1d8ddf1440f1ac9b9a660fd017ce9c3e
unknown <sergiumarcu97@gmail.com>
2023-11-28 03:35:26 +0200

    dark mode time

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div className="w-full bg-slate-100 text-zinc-800">
+    <div className="w-full bg-purple-500 text-zinc-800 z-50 dark:bg-gray-900 dark:text-white">
         <div className="mx-auto max-w-6xl py-2 lg:py-4">
           <Link href={"/"} className="px-4 text-lg">eMenu</Link>
         </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import EMenuUserButton from "./Header/EMenuUserButton";
+import { ThemeToggler } from "./ui/ThemeToggler";
 
 const Header = () => {
     return (
-      <div className="w-full bg-slate-100 text-zinc-800 z-50">
+      <div className="w-full bg-purple-500 text-zinc-800 z-50 dark:bg-gray-900 dark:text-white">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 py-4 flex items-center justify-between">
           <Link href={"/"} className="px-4 text-lg">eMenu</Link>
+          <div className="flex items-center gap-4">
+          <ThemeToggler />
           <EMenuUserButton />
+          </div>
         </div>
       </div>
     )
