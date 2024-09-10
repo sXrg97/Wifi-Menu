f5e0af8479f29d1be6512b0411d6b569f3110259
unknown <sergiumarcu97@gmail.com>
2024-09-10 23:29:46 +0300

    whatsapp number fix

--- components/Whatsapp.tsx
+++ components/Whatsapp.tsx
@@ -4,7 +4,7 @@ import { FloatingWhatsApp } from "react-floating-whatsapp"
 
 const Whatsapp = () => {
   return (
-    <FloatingWhatsApp  notificationSound={true} notification={true} notificationDelay={1} notificationLoop={1} accountName="Wifi Menu" phoneNumber="0040773784220" avatar="/wifi-menu-logo-white-on-purple-bg.png" chatMessage="Buna ziua! Cu ce va putem ajuta?" />
+    <FloatingWhatsApp  notificationSound={true} notification={true} notificationDelay={1} notificationLoop={1} accountName="Wifi Menu" phoneNumber="40773784220" avatar="/wifi-menu-logo-white-on-purple-bg.png" chatMessage="Buna ziua! Cu ce va putem ajuta?" />
   )
 }
 
