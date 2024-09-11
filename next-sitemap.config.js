72b9fede36420846d775b104b53a9a3a3bdf839e
unknown <sergiumarcu97@gmail.com>
2024-09-11 13:27:46 +0300

    new sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,30 @@
 module.exports = {
-    siteUrl: 'https://wifi-menu.ro',
-    generateRobotsTxt: true,
-    exclude: ['/server-sitemap.xml'], // Exclude dynamic sitemap from static generation
-    robotsTxtOptions: {
-      additionalSitemaps: [
-        'https://wifi-menu.ro/server-sitemap.xml', // Add dynamic sitemap here
-      ],
-      policies: [
-        {
-            userAgent: '*',
-            disallow: ['/dashboard']
-        }
-      ]
-    },
-  }
+  siteUrl: "https://wifi-menu.ro",
+  generateRobotsTxt: true,
+  exclude: ["/server-sitemap.xml"], // Exclude dynamic sitemap from static generation
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://wifi-menu.ro/server-sitemap.xml", // Add dynamic sitemap here
+    ],
+    policies: [
+      {
+        userAgent: "*",
+        disallow: [
+          "/dashboard",
+          "/*.json$",
+          "/*_buildManifest.js$",
+          "/*_middlewareManifest.js$",
+          "/*_ssgManifest.js$",
+          "/*.js$",
+        ],
+        allow: [
+          "/",
+          "/cookie-policy",
+          "/refund-policy",
+          "/privacy-policy",
+          "/terms-and-conditions",
+        ],
+      },
+    ],
+  },
+};
