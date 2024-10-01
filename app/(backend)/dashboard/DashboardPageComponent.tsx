4cc540794bb2acb11e7fda13d9b24707fa52e7d4
unknown <sergiumarcu97@gmail.com>
2024-10-01 17:25:49 +0300

    video tutorial

--- app/(backend)/dashboard/DashboardPageComponent.tsx
+++ app/(backend)/dashboard/DashboardPageComponent.tsx
@@ -1,18 +1,18 @@
 'use client'
 
 import { useEffect, useState, useCallback } from 'react'
-import { useRouter } from 'next/navigation'
 import RestaurantInfo from '@/components/Dashboard/RestaurantInfo'
 import { checkUserOrCreate } from '@/lib/actions/user.actions'
 import { useUser } from '@clerk/nextjs'
 import RestaurantSetup from '@/components/Backend/RestaurantSetup'
 import { Loader } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const DashboardPageComponent = () => {
     const [menuId, setMenuId] = useState<string | null>(null)
     const [loading, setLoading] = useState(true)
     const { isLoaded, isSignedIn, user } = useUser()
-    const router = useRouter()
   
     const createOrGetMenu = useCallback(async () => {
       if (isSignedIn) {
@@ -47,6 +47,18 @@ const DashboardPageComponent = () => {
   
     return (
       <main>
+        <div className="bg-purple-100 p-4 text-center">
+            <p className="text-lg flex flex-col gap-2 items-center">
+                <span>Dacă ai nevoie de ajutor cu platforma, nu ezita să urmărești videoclipul nostru!</span>
+
+                <Link href={'/dashboard/tutorial-dashboard'} className='flex-1 w-full'>
+                  <Button variant={'default'} className="text-white w-full">
+                        Vizionare videoclip
+                  </Button>
+                </Link>
+            </p>
+        </div>
+
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 md:px-8">
           <h1 className='text-4xl font-bold mb-4 dashboard-title'>
             Dashboard
---
+++ app/(backend)/dashboard/tutorial-dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TutorialDashboard = () => {
+  return (
+    <div className='flex flex-col items-center gap-4 p-4 md:p-8 max-w-6xl mx-auto'>
+    <h1 className='text-3xl my-2 font-bold'>Tutorial Wifi Menu</h1>
+
+    <div><iframe className='w-full aspect-[9/16]' src="https://player.vimeo.com/video/1014828074?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="Wifi Menu - Cum sa iti creezi propriul meniu digital"></iframe></div>
+    </div>
+  )
+}
+
+export default TutorialDashboard
